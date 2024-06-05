44b8e029e17dad65f903496562dad5cd96fe1ac4
tjohoski3165 <tjohoski3165@studnet.pps.net>
2024-06-05 11:39:19 -0700

    k

--- Final Project/Levels/LevelManager.java
+++ Final Project/Levels/LevelManager.java
@@ -9,15 +9,17 @@ import java.awt.image.BufferedImage;
 public class LevelManager {
     private Game game;
     private BufferedImage[] leveSprite;
+
+
     public LevelManager(Game game){
         this.game = game;
 //        leveSprite = LoadSave.GetPlayerAboogRetep(LoadSave.LEVEL_ABMOOG_RETEP);
-        importBse();
+        importSprites();
 
     }
 
-    private void importBse() {
-        leveSprite = new BufferedImage[];
+    private void importSprites() {
+        leveSprite = new BufferedImage[4];
     }
 
     public void draw(Graphics g) {
